b7ef5eff622af8bb0caf70d0968cf7ec743df3df
Ayush <ayushadarsh03@gmail.com>
2020-12-10 18:19:30 +0530

    Listings app created and skeletally functional

---
+++ listings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views as views
+
+urlpatterns = [
+     path('',views.index, name='listings'),                                                 # For /listings/ page
+     path('<int:listing_id>',views.listing, name='listing'),                               # For listings/4 page
+     path('search',views.search, name='search'),                                            # For search handling on page
+     
+]
