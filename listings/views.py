4d09b0b83d40be697d27118f8c6d81facb47d251
Ayush <ayushadarsh03@gmail.com>
2021-01-06 18:12:19 +0530

    Added Pagination on Listings and handled a bit of Filtering data acc to boolean value in DB

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    # order_by('-list_date') = views the listings according to the date they were published. Last In First Out
+    # filter(is_published=True) = Filter data according to the is_published value in the DB
+
+    
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render (request,'listings/listings.html',context)
