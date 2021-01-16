d52d26d69f156239ade15581f54c20f3bf3ce5ab
Ayush <ayushadarsh03@gmail.com>
2021-01-16 22:15:50 +0530

    Made the UI changes & basic funcs for Register & Login pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 
 def register(request):
-    return render(request, 'accounts/register.html')
+
+    # checking if the request type is GET or POST
+    if request.method == 'POST':
+        print(request)
+        print("Request is submitted")
+        # print("Request is submitted " + request) This is not allowed
+        # i.e. - Mixed static and dynamic values
+        # return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
+    if request.method == 'POST':
+        print(request)
+        # Login USer 
+    else:
+        return render(request, 'accounts/login.html')
+
     return render(request, 'accounts/login.html')
 
 def logout(request):
