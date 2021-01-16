f0ac883d6fc9c285d51ffb6df7c67ea0a767e061
Ayush <ayushadarsh03@gmail.com>
2021-01-16 21:14:43 +0530

    Basic Search Functionality added for all fields of Listing

--- listings/views.py
+++ listings/views.py
@@ -32,10 +32,48 @@ def listing(request, listing_id):
 
 def search(request):
 
+    queryset_List = Listing.objects.order_by('-list_date')
+    
+    # Different sections acc to searchable items. Refer HTML for more understranding
+    
+    # For Keywords - String/text field
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_List = queryset_List.filter(description__icontains=keywords)
+
+    # For City - String/text field
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_List = queryset_List.filter(city__iexact=city)
+
+    # For State - String/text field
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_List = queryset_List.filter(state__iexact=state)
+
+    # For Bedrooms - Number
+    # lte = Less than or Equal To
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_List = queryset_List.filter(bedrooms__lte=bedrooms)
+
+    # For Price - Number
+    # lte = Less than or Equal To
+    if 'price' in request.GET:
+        price = request.GET['price']
+        # print(price) Logging is visible on Server Logs
+        if price:
+            queryset_List = queryset_List.filter(price__lte=price)
+
     context = {
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
-        'state_choices': state_choices
+        'state_choices': state_choices,
+        'listings': queryset_List
         
     }
     return render (request,'listings/search.html',context)
