821467c9a73f7d0143ffc209d8fb140b8a0d161a
Ayush <ayushadarsh03@gmail.com>
2021-01-16 21:52:22 +0530

    Adding new App for User login/register and creating the skeleton code

--- btre/urls.py
+++ btre/urls.py
@@ -6,7 +6,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+
+    # All the apps must have their urls.py entries here too
+
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),    
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,7 @@ from . import views as views
 
 urlpatterns = [
      path('',views.index, name='listings'),                                                 # For /listings/ page
-     path('<int:listing_id>',views.listing, name='listing'),                               # For listings/4 page
+     path('<int:listing_id>',views.listing, name='listing'),                                # For listings/4 page
      path('search',views.search, name='search'),                                            # For search handling on page
      
 ]
