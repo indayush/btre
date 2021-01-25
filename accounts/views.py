ea9200a13b1272a7248262bcd4ccbe86d3fe4276
Ayush <ayushadarsh03@gmail.com>
2021-01-25 13:53:07 +0530

    User Login after Registeration implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def register(request):
 
     # checking if the request type is GET or POST
     if request.method == 'POST':
-        print(request)
-        print("Request is submitted")
+        # print(request)
+        # print("Request is submitted")
         # print("Request is submitted " + request) This is not allowed
         # i.e. - Mixed static and dynamic values
         # return redirect('register')
+
+        # Alternate way of doing this -
+        # first_name = request.POST.get("first_name")
+        # last_name = request.POST.get("last_name")
+        # username = request.POST['username']
+        # email = request.POST.get("email")
+        # password = request.POST['password']
+        # password2 = request.POST.get("password2")
 
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -41,9 +49,6 @@ def register(request):
                     return redirect('index')
                     '''
         else:
-            print('password = ' + password)
-            print('password2 = ' + password2)
-
             messages.error(request, 'Passwords do not match')
             return redirect('register')
        
@@ -51,13 +56,30 @@ def register(request):
         return render(request, 'accounts/register.html')
 
 def login(request):
+    
+    # print('================================')
+    # print('Inside LOGIN method')
+    # print(request.method)
+
     if request.method == 'POST':
-        print(request)
-        # Login User 
+
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
-
-    return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
