a14bcdd9512ebd5bfdd57ec90b4a33eb09695353
Ayush <ayushadarsh03@gmail.com>
2021-01-25 11:10:34 +0530

    Static Files Importing issue resolved

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,6 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
-// Function to fadeout Error Message - NOT working Though
-
-setTimeout(function(){
+setTimeout(function() {
     $('#message').fadeOut('slow');
-}, 4000);
+},3000);
